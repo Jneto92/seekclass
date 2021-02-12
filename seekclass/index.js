3aec3c16d668ac45b0145909432ab9c3817c79fa
Jneto92 <55671411+Jneto92@users.noreply.github.com>
2021-02-12 01:38:25 -0300

    versão 1.1

--- seekclass/index.js
+++ seekclass/index.js
@@ -1,11 +1,44 @@
 const express = require('express');
 const app = express();
+const handlebars = require('express-handlebars');
+const bodyParser = require('body-parser');
+const Usuario = require('./models/Usuario');
 
-app.set('view engine', 'ejs');
+//config
+    //Body parser
+    app.use(bodyParser.urlencoded({extended: false}));
+    app.use(bodyParser.json());
+    //template engine
+    app.engine('handlebars', handlebars({defaultLayout: 'main'}));
+    app.set('view engine', 'handlebars');
 
+//rotas
+       
 
+    app.get('/cadastrar', (req, res)=>{
+        res.render('cadastro')
+    });
+    app.get('/', (req, res)=>{
+        res.render('login')
+    });
+    app.get('/first', (req, res)=>{
+        res.render('primeiroacesso')
+    });
+    app.post('/cadastrar', (req, res)=>{
 
-app.get("/",(req, res) => {
+        Usuario.create({
+            nome: req.body.nome,
+            email: req.body.email
+        }).then(()=>{
+            //res.send("Usuário cadastrado")
+            res.redirect('/login');
+        }).catch((erro)=>{
+            res.send("Erro na criação do usuário."+erro);
+        });
+        
+    });
+
+/*app.get("/",(req, res) => {
     //res.render("../views/index");
     res.sendFile(__dirname + "/html/index.html");
     
@@ -30,7 +63,7 @@ app.get("/home",(req, res) => {
 
 function cadastrar(){
     res.sendFile(__dirname + "/html/cadastro.html");
-};
+};*/
 app.listen(8080, () => {
     console.log('Executando')
 });
---
+++ seekclass/models/Usuario.js
@@ -0,0 +1,20 @@
+const db = require('./db');
+
+const Usuario = db.sequelize.define('usuarios', {
+    nome:{
+        type: db.Sequelize.STRING(50),
+        allowNull: false
+    },
+    email:{
+        type: db.Sequelize.STRING,
+        unique: "email_user",
+        allowNull: false
+    },
+    senha:{
+        type: db.Sequelize.STRING
+    }
+});
+
+//Usuario.sync({force: true});
+
+module.exports = Usuario;
